d4107fc9bc6ce18d662984f34300fb3eb1c3fb04
magic.chen <magic@magicchendeMacBook-Pro.local>
2018-07-06 16:52:55 +0800

    merkle tree impletement

--- utils/merkletree.py
+++ utils/merkletree.py
@@ -3,6 +3,71 @@
 
 """
 This Document is Created by  At 2018/6/30 
+This module is merkletree implement
 """
 
+import hashlib
 
+
+class MerkleTree:
+    """
+    - doc  Merkle tree
+    """
+
+    def __init__(self, data):
+        if not isinstance(data, list):
+            raise TypeError("data must be a bytes list")
+
+        self.data = data
+        self.nodes = []
+        self.root = None
+
+    def new_tree(self):
+
+        if len(self.data) % 2 != 0:
+            self.data.append(self.data[-1])
+
+        for d in self.data:
+
+            if isinstance(d, bytes) or isinstance(d, bytearray):
+
+                node = MerkleNode(None, None, d)
+                node.new_node()
+                self.nodes.append(node)
+            else:
+                raise TypeError("数据类型错误！")
+
+        for i in range(int(len(self.data)/2)):
+            new_level = []
+
+            for j in range(0, len(self.nodes), 2):
+                node = MerkleNode(self.nodes[j], self.nodes[j + 1], b'')
+                node.new_node()
+                new_level.append(node)
+            self.nodes = new_level
+
+        self.root = self.nodes[0]
+
+
+class MerkleNode:
+    """
+    - doc
+    """
+
+    def __init__(self, left, right, data):
+        self.Left = left
+        self.Right = right
+
+        if isinstance(data, bytes) or isinstance(data, bytearray):
+            self.data = data
+        else:
+            raise TypeError("data 不是byte类型!")
+
+    def new_node(self):
+        if not self.Left and not self.Right:
+            hash_value = hashlib.sha256(bytes(self.data)).hexdigest()
+            self.data = hash_value
+        else:
+            prehash = self.Left.data + self.Right.data
+            hash_value = hashlib.sha256(prehash.encode()).hexdigest()
+            self.data = hash_value
---
+++ utils/merkletree_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+This Document is Created by  At 2018/7/6 
+"""
+from utils.merkletree import MerkleNode, MerkleTree
+
+if __name__ == "__main__":
+    m1 = MerkleNode(None, None, b"Node1")
+    m1.new_node()
+
+    m2 = MerkleNode(None, None, b"Node2")
+    m2.new_node()
+
+    m3 = MerkleNode(None, None, b"Node3")
+    m3.new_node()
+
+    m4 = MerkleNode(None, None, b"Node3")
+    m4.new_node()
+
+    m5 = MerkleNode(m1, m2, b'')
+    m5.new_node()
+    print(m5.Left, m5.Right, m5.data)
+
+    tree = MerkleTree([b'Node1', b'Node2', b'Node3'])
+    tree.new_tree()
+
+    print(tree.root.data)
+
+
+
+
