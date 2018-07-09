1c9f2a2065331a77ee4fe29e4c8b6d71d87644fe
magic.chen <magic@magicchendeMacBook-Pro.local>
2018-07-09 18:20:53 +0800

    rpc 通信部分实现

--- net/__init__.py
+++ net/__init__.py
@@ -2,7 +2,9 @@
 # -*- coding:utf-8 -*-
 
 """
-This Document is Created by  At 2018/6/29 
+This Document is Created by  At 2018/6/29
+
+区块链网络通信部分
 """
 
 
---
+++ rpc/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+This Document is Created by  At 2018/7/9
+
+打算用rpc来进行通信。 使用grpc库来实现此部分
+"""
+
+
