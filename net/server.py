2ce1e627131aebaf6d2938163e29b2208bf2336c
magic.chen <magic@magicchendeMacBook-Pro.local>
2018-07-11 16:59:08 +0800

    add flask and test server

--- net/server.py
+++ net/server.py
@@ -5,4 +5,15 @@
 This Document is Created by  At 2018/7/11 
 """
 
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run(port=8888, debug=True)
