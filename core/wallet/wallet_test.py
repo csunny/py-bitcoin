6a86956f661c13df99d46b2a3056973ab7eeb468
csunny <cfqcsunny@gmail.com>
2018-07-17 10:42:56 +0800

    add todo list

--- core/wallet/wallet_test.py
+++ core/wallet/wallet_test.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 """
-This Document is Created by  At 2018/7/8 
+This Document is Created by  At 2018/7/8
 """
 from core.wallet.wallet import Wallet
 
@@ -10,6 +10,10 @@ if __name__ == "__main__":
     w = Wallet()
 
     # todo need to test
+    # there is something error in th test,
+    # sometime the assert is ok, but others not.
+    # so,maybe something need to do to find the bug.
+    # Mark .
     address = w.get_address()
     valid = w.validate_addr(address)
 
