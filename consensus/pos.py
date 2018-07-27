26b735f81216eee3696f264184265f64b580cea0
csunny <cfqcsunny@gmail.com>
2018-07-27 15:02:54 +0800

    complete pos (proof of stake)

--- consensus/pos.py
+++ consensus/pos.py
@@ -88,7 +88,6 @@ def pick_winner(announcements):
     time.sleep(10)
 
     while True:
-
         with My_Lock:
             temp = temp_blocks
 
@@ -100,10 +99,11 @@ def pick_winner(announcements):
                     set_validators = validators
                     k = set_validators.get(block["Validator"])
                     if k:
-                        lottery_pool.append(block["Validator"] for _ in k)
+                        for i in range(k):
+                            lottery_pool.append(block["Validator"])
 
             lottery_winner = choice(lottery_pool)
-
+            print(lottery_winner)
             # add block of winner to blockchain and let all the other nodes known
             for block in temp:
                 if block["Validator"] == lottery_winner:
@@ -111,10 +111,8 @@ def pick_winner(announcements):
                         block_chain.append(block)
 
                     # write message in queue.
-
-                    for _ in range(len(validators)):
-                        msg = "\n:{0} 赢得了记账权利\n".format(lottery_winner)
-                        announcements.put(msg)
+                    msg = "\n{0} 赢得了记账权利\n".format(lottery_winner)
+                    announcements.put(msg)
 
                     break
 
@@ -125,33 +123,27 @@ def pick_winner(announcements):
 class HandleConn(BaseRequestHandler):
     def handle(self):
         print("Got connection from", self.client_address)
+
+        # validator address
+        self.request.send(b"Enter token balance:")
+        balance = self.request.recv(8192)
+        try:
+            balance = int(balance)
+        except Exception as e:
+            print(e)
+
+        t = str(datetime.now())
+        address = sha256(t.encode()).hexdigest()
+        validators[address] = balance
+        print(validators)
+
         while True:
-
-            # todo a thread sync info to client always
-            try:
-                msg = announcements.get(block=False)
-                self.request.send(msg.encode())
-            except Empty:
-                pass
-
-            # validator address
-            self.request.send(b"Enter token balance:")
-            balance = self.request.recv(8192)
-
-            try:
-                balance = int(balance)
-            except Exception as e:
-                print(e)
-                break
-
-            t = str(datetime.now())
-            address = sha256(t.encode()).hexdigest()
-            validators[address] = balance
-            print(validators)
+            announce_winner_t = threading.Thread(target=annouce_winner, args=(announcements, self.request,),
+                                                 daemon=True)
+            announce_winner_t.start()
 
             self.request.send(b"\nEnter a new BPM:")
             bpm = self.request.recv(8192)
-            # todo a thread to generate a  new block
             try:
                 bpm = int(bpm)
             except Exception as e:
@@ -164,26 +156,61 @@ class HandleConn(BaseRequestHandler):
 
             new_block = generate_block(last_block, bpm, address)
 
-            print(new_block, last_block)
             if is_block_valid(new_block, last_block):
+                print("new block is valid!")
                 candidate_blocks.put(new_block)
 
-            self.request.send(b"\nEnter a new BPM:")
+            self.request.send(b"\nEnter a new BPM:\n")
 
-            # todo a thread always send msg to client
-            time.sleep(30)
-            with My_Lock:
-                output = json.dumps(block_chain)
+            annouce_blockchain_t = threading.Thread(target=annouce_blockchain, args=(self.request,), daemon=True)
+            annouce_blockchain_t.start()
 
-            self.request.send(output.encode())
+
+def annouce_winner(announcements, request):
+    """
+
+    :param announcements:
+    :param request:
+    :return:
+    """
+    while True:
+        try:
+            msg = announcements.get(block=False)
+            request.send(msg.encode())
+            request.send(b'\n')
+        except Empty:
+            time.sleep(3)
+            continue
+
+
+def annouce_blockchain(request):
+    """
+
+    :param request:
+    :return:
+    """
+    while True:
+        time.sleep(30)
+        with My_Lock:
+            output = json.dumps(block_chain)
+        try:
+            request.send(output.encode())
+            request.send(b'\n')
+        except OSError:
+            pass
 
 
 def candidate(candidate_blocks):
+    """
+
+    :param candidate_blocks:
+    :return:
+    """
     while True:
         try:
             candi = candidate_blocks.get(block=False)
         except Empty:
-            time.sleep(3)
+            time.sleep(5)
             continue
         temp_blocks.append(candi)
 
@@ -203,16 +230,15 @@ def run():
     print(genesis_block)
     block_chain.append(genesis_block)
 
-    # start a tcp server
-    serv = ThreadingTCPServer(('', 9090), HandleConn)
-    serv.serve_forever()
-
-    thread_canditate = threading.Thread(target=candidate, args=(candidate_blocks,))
-    thread_pick = threading.Thread(target=pick_winner, args=(announcements,))
+    thread_canditate = threading.Thread(target=candidate, args=(candidate_blocks,), daemon=True)
+    thread_pick = threading.Thread(target=pick_winner, args=(announcements,), daemon=True)
 
     thread_canditate.start()
     thread_pick.start()
 
+    # start a tcp server
+    serv = ThreadingTCPServer(('', 9090), HandleConn)
+    serv.serve_forever()
 
 if __name__ == '__main__':
     run()
